85a401e0005f296886d248235a0fbfab2a1d6740
Anhqvv <anhqvv2022@gmail.com>
2023-04-12 18:38:57 +0700

    #4-1 #4-2 React Router & chia layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,11 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
       </React.StrictMode>
     </Provider>
   </React.StrictMode>
---
+++ src/pages/book/index.jsx
@@ -0,0 +1,5 @@
+const BookPage = () => {
+  return <div>BookPage component</div>
+}
+
+export default BookPage
